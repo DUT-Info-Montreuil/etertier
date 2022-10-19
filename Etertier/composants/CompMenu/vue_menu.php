e7e88062a2ed58b4f4fafa86a59113ff3bab9838
TheblackReaper060303 <96045738+TheblackReaper060303@users.noreply.github.com>
2022-10-19 16:05:42 +0200

    vue menu change 2

--- Etertier/composants/CompMenu/vue_menu.php
+++ Etertier/composants/CompMenu/vue_menu.php
@@ -5,11 +5,26 @@
 		public $contenu;
 
 		public function __construct (){
-			$this->contenu = '<nav>';
+			$this->contenu = '<div>';
 				if(!isset($_SESSION['login'])){
-					
-					$this->contenu .= "<a href=index.php?module=connexion&action=connexion>Connexion</a> <a href=index.php?module=connexion&action=inscription>Inscription</a></nav>";
-					
+					$this->contenu .= 
+					"<nav>
+						<ul class=\"nav justify-content-center\">
+							<li class=\"nav-item\"><a class=\"nav-link\" href=\"#\">Acceuil</a></li>
+							<li class=\"nav-item\"><a class=\"nav-link\" href=\"#\">Jeux</a></li>
+							<li class=\"nav-item\"><a class=\"nav-link\" href=\"#\">Liste Rédacteurs</a></li>
+							<li class=\"nav-item\"><a class=\"nav-link\" href=\"#\">Liste Membres</a></li>
+							<li class=\"nav-item\"><a class=\"nav-link\" href=\"#\">Articles</a></li>
+							<li class=\"nav-item\"><a class=\"nav-link\" href=\"#\">FAQ</a></li>
+						</ul>
+				  </nav>";
+				  $this->contenu .= 
+				  "<nav>
+					  <ul class=\"nav justify-content-end\">
+					  		<li class=\"nav-item\"><a class=\"nav-link\" href=index.php?module=connexion&action=connexion>Connexion</a></li>
+						  	<li class=\"nav-item\"><a class=\"nav-link\" href=index.php?module=connexion&action=inscription>Inscription</a></li>
+					  </ul>
+				  </nav>";
 				} else{
 					$this->contenu .= "<i class=\"fa-solid fa-user\"></i>" . $_SESSION['login']." - <a href=index.php?module=connexion&action=deconnexion>Déconnexion</a></nav>";
 				}
